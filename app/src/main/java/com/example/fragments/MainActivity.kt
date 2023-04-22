ca0fc1e4604a4a3b689b797b2f1b45f43dbebd28
Shivangi <shivangit1111@gmail.com>
2023-04-22 14:23:33 +0530

    feat: open fragment on button click

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        loadFragment(FragmentA())
+
+        binding.button.setOnClickListener {
+            loadFragment(FragmentA())
+        }
+
+        binding.button2.setOnClickListener {
+            loadFragment(FragmentB())
+        }
+
+        binding.btn3.setOnClickListener {
+            loadFragment(FragmentC())
+        }
+
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+
 }
